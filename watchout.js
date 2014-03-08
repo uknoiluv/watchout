27b6bdbe56a0e43989ad29f85ba107c3ab9dde75
Hack Reactor Students <uknoiluvu@gmail.com>
2014-03-07 20:14:53 -0800

    in progress

--- watchout.js
+++ watchout.js
@@ -1,32 +1,82 @@
 // start slingin' some d3 here.
 
-var circlesArray = function(num, size){
+
+var width = 600;
+var height = 600;
+var size = 10;
+
+var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("background", "#ccc");
+  // .append("g");
+
+
+// svg.append("rect")
+//   .attr("width", width)
+//   .attr("height", height)
+//   .attr("fill", "#ccc");
+
+
+var update = function(data) {
+
+  svg.selectAll('circle').data(data).enter()
+
+
+  var enemies = svg.selectAll("circle").data(data);
+
+  enemies.attr("class","update");
+
+  enemies.enter().append("enemies")
+    .attr("class", "enter")
+    .attr("cx", function(d){return d.x;})
+    .attr("cy", function(d){return d.y;})
+    .attr("r", size);
+  // enemies.enter().append("circle");
+  enemies.exit().remove();
+
+};
+
+setInterval(function(){
+  update(randomPosition(30));
+}, 500);
+
+var randomPosition = function(num){
   var array = [];
+  var posObj ={};
   for (var i = 0; i < num; i++){
-    array.push(size);
+    posObj['x'] = (Math.random()*600);
+    posObj['y'] = (Math.random()*600);
+    array.push(posObj)
   }
+  console.log(array);
   return array;
+  // return posObj;
 };
 
 
 
-var svg = d3.selectAll('.circles').append('svg');
-var w = window.innerWidth, h = window.innerHeight;
-svg.attr({width:w, height:h });
-svg.selectAll('circle').data(circlesArray(30, 10)).enter().append('circle').attr('r', function(d){return d;})
-  .attr('cx', function(d){return Math.random() * w})
-  .attr('cy', function(d){return Math.random() * h})
-  .data(['#fcc']).attr('color', function(d){return d});
+// var circlesArray = function(num, size){
+//   var array = [];
+//   for (var i = 0; i < num; i++){
+//     array.push(size);
+//   }
+//   return array;
+// };
+
+// var count = 0;
+// var circleMaker = function(){
+//   console.log(count++);
+// var svg = d3.selectAll('.circles').append('svg');
+// var w = window.innerWidth, h = window.innerHeight;
+// svg.attr({width:w, height:h });
+// svg.selectAll('circle').data(circlesArray(30, 10)).enter().append('circle').attr('r', function(d){return d;})
+//   .attr('cx', function(d){return Math.random() * w;})
+//   .attr('cy', function(d){return Math.random() * h;})
+//   .data(['#fcc']).attr('color', function(d){return d;});
+//   setTimeout(circleMaker, 500);
+// };
 
 
+// circleMaker();
 
-
-// svg.selectAll('circle').data(['blue', 'yellow', 'green']).enter().append('circle').attr('color', function(d){return d;});
-
-
-// d3.selectAll('div').data([18, 4, 7]);
-// d3.selectAll('div').data(['red', 'green', 'blue']).style('color', function(d){ return d; });
-
-
-// d3.selectAll('div').data([18, 4, 7]).style('width', function(d){return d + '%'});
-// d3.selectAll('div').data(['yellow', 'purple', 'pink']).style('color', function(d){ return d; });
